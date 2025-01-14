9445c9b1337d00eb1407e2c159dc283b82538557
patrycjaoczkos <poczkos@st.swps.edu.pl>
2025-01-14 18:35:01 +0100

    lab_07

--- folder_projektu/folder_aplikacji/serializers.py
+++ folder_projektu/folder_aplikacji/serializers.py
@@ -6,9 +6,17 @@ class PersonSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=True)
     shirt_size = serializers.ChoiceField(choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
-    miesiac_dodania = serializers.ChoiceField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
+    month_added = serializers.ChoiceField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
     team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())
     pseudonim = serializers.CharField(required=False)
+    
+    def validate_name(self, value):
+
+        if not value.istitle():
+            raise serializers.ValidationError(
+                "Nazwa osoby powinna rozpoczynać się wielką literą!",
+            )
+        return value
 
     def create(self, validated_data):
         return Person.objects.create(**validated_data)
@@ -16,7 +24,7 @@ class PersonSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.shirt_size = validated_data.get('shirt_size', instance.shirt_size)
-        instance.miesiac_dodania = validated_data.get('miesiac_dodania', instance.miesiac_dodania)
+        instance.month_added = validated_data.get('month_added', instance.month_added)
         instance.team = validated_data.get('team', instance.team)
         instance.pseudonim = validated_data.get('pseudonim', instance.pseudonim)
         instance.save()
